439c7574f026e1d09eafffb54385078774b40d4a
NotTimIsReal <alastairrintumathew@gmail.com>
2024-01-04 21:30:58 +1100

    Ported to winit

--- build.rs
+++ build.rs
@@ -1,1 +1,7 @@
-fn main() {}
+fn main() {
+    #[cfg(any(target_os = "openbsd", target_os = "freebsd"))]
+    println!(r"cargo:rustc-link-search=/usr/local/lib");
+
+    #[cfg(all(target_os = "macos", feature = "use_mac_framework"))]
+    println!("cargo:rustc-link-search=framework=/Library/Frameworks")
+}
--- src/window.rs
+++ src/window.rs
@@ -1,89 +1,78 @@
-use sdl2::{self, video::Window};
-
 use crate::renderer::Renderer;
+use winit::{
+    event::{ElementState, Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    keyboard::Key,
+    platform::{macos::WindowExtMacOS, modifier_supplement::KeyEventExtModifierSupplement},
+    window::{Fullscreen, WindowBuilder},
+};
 
 pub struct Engine {
-    sdl_context: sdl2::Sdl,
-    video_subsystem: sdl2::VideoSubsystem,
-    //canvas: &'a sdl2::render::Canvas<sdl2::video::Window>,
     renderer: Renderer,
+    event_loop: EventLoop<()>,
 }
 impl Engine {
     pub async fn new() -> Engine {
-        let sdl_context = sdl2::init().unwrap();
-        let video_subsystem = sdl_context.video().unwrap();
-        // Set the hint based on the platform
-        let mut windowbuilder = video_subsystem.window("Game", 800, 600);
-
-        #[cfg(target_os = "macos")]
-        windowbuilder.metal_view();
-        #[cfg(target_os = "windows")]
-        windowbuilder.vulkan();
-        #[cfg(target_os = "linux")]
-        windowbuilder.opengl();
-        let window = windowbuilder
-            .fullscreen_desktop()
-            .position_centered()
-            .build()
+        let event_loop = EventLoop::new().unwrap();
+        let window = WindowBuilder::new()
+            .with_title("Game")
+            .build(&event_loop)
             .unwrap();
-
-        #[cfg(target_os = "macos")]
-        sdl2::hint::set("SDL_RENDER_DRIVER", "metal");
-
-        #[cfg(target_os = "windows")]
-        sdl2::hint::set("SDL_RENDER_DRIVER", "opengl");
-
-        #[cfg(target_os = "linux")]
-        sdl2::hint::set("SDL_RENDER_DRIVER", "vulkan");
+        window.set_fullscreen(Some(Fullscreen::Borderless(window.current_monitor())));
         let renderer = Renderer::new(window).await;
-        // let canvas = &window.into_canvas().build().unwrap();
-
         Engine {
-            sdl_context,
-            video_subsystem,
-            // canvas,
             renderer,
+            event_loop,
         }
         //depending on the platform, use vulkan,opengl or metal
     }
-    pub fn run(&mut self) {
-        let mut event_pump = self.sdl_context.event_pump().unwrap();
+    pub fn run(mut self) {
+        self.event_loop
+            .run(move |event, elwt| {
+                if let Event::WindowEvent { event, .. } = event {
+                    match event {
+                        WindowEvent::KeyboardInput { event, .. } => {
+                            if event.state == ElementState::Pressed {
+                                match event.key_without_modifiers().as_ref() {
+                                    Key::Character("w") => {
+                                        self.renderer.camera_controller.process_keyboard(
+                                            crate::camera::CameraMovement::Forward,
+                                        );
+                                    }
+                                    Key::Character("a") => {
+                                        self.renderer
+                                            .camera_controller
+                                            .process_keyboard(crate::camera::CameraMovement::Left);
+                                    }
+                                    Key::Character("s") => {
+                                        self.renderer.camera_controller.process_keyboard(
+                                            crate::camera::CameraMovement::Backward,
+                                        );
+                                    }
+                                    Key::Character("d") => {
+                                        self.renderer
+                                            .camera_controller
+                                            .process_keyboard(crate::camera::CameraMovement::Right);
+                                    }
+                                    _ => {}
+                                }
+                            }
+                        }
+                        WindowEvent::Resized(physical_size) => {
+                            self.renderer.resize(physical_size);
+                        }
+                        WindowEvent::CloseRequested => {
+                            elwt.exit();
+                        }
 
-        'running: loop {
-            for event in event_pump.poll_iter() {
-                match event {
-                    sdl2::event::Event::Quit { .. } => break 'running,
-                    sdl2::event::Event::KeyDown { keycode, .. } => match keycode {
-                        Some(sdl2::keyboard::Keycode::W) => {
-                            self.renderer
-                                .camera_controller
-                                .process_keyboard(crate::camera::CameraMovement::Forward);
-                        }
-                        Some(sdl2::keyboard::Keycode::A) => {
-                            self.renderer
-                                .camera_controller
-                                .process_keyboard(crate::camera::CameraMovement::Left);
-                        }
-                        Some(sdl2::keyboard::Keycode::S) => {
-                            self.renderer
-                                .camera_controller
-                                .process_keyboard(crate::camera::CameraMovement::Backward);
-                        }
-                        Some(sdl2::keyboard::Keycode::D) => {
-                            self.renderer
-                                .camera_controller
-                                .process_keyboard(crate::camera::CameraMovement::Right);
-                        }
+                        //redraw requested
                         _ => {}
-                    },
-                    //redraw requested
-                    _ => {}
+                    }
                 }
-
                 self.renderer.update();
                 self.renderer.render();
-            }
-        }
+            })
+            .unwrap();
         //cleanup
     }
 }
