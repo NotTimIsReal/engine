eaa270171cb86308f45d5c902679cf6c1d1f3c52
NotTimIsReal <alastairrintumathew@gmail.com>
2024-02-23 17:45:04 +1100

    finished camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,5 +1,11 @@
-use cgmath::InnerSpace;
+use std::f32::consts::FRAC_PI_2;
+use std::time::Duration;
+
 use cgmath::{self, Vector3};
+use cgmath::{InnerSpace, Rad};
+use winit::dpi::PhysicalPosition;
+use winit::event::{ElementState, MouseScrollDelta};
+use winit::keyboard::{self, Key, ModifiersState};
 #[rustfmt::skip]
 const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
     1.0, 0.0, 0.0, 0.0,
@@ -7,21 +13,58 @@ const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
     0.0, 0.0, 0.5, 0.5,
     0.0, 0.0, 0.0, 1.0,
 );
-
+const SAFE_FRAC_PI_2: f32 = FRAC_PI_2 - 0.0001;
 pub struct Camera {
-    pub eye: cgmath::Point3<f32>,
-    pub target: cgmath::Point3<f32>,
-    pub up: cgmath::Vector3<f32>,
-    pub aspect: f32,
-    pub fovy: f32,
-    pub znear: f32,
-    pub zfar: f32,
+    pub position: cgmath::Point3<f32>,
+    yaw: cgmath::Rad<f32>,
+    pitch: cgmath::Rad<f32>,
 }
 impl Camera {
-    fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
-        let view = cgmath::Matrix4::look_at_rh(self.eye, self.target, self.up);
-        let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
-        return OPENGL_TO_WGPU_MATRIX * proj * view;
+    pub fn new<
+        P: Into<cgmath::Point3<f32>>,
+        Y: Into<cgmath::Rad<f32>>,
+        I: Into<cgmath::Rad<f32>>,
+    >(
+        position: P,
+        yaw: Y,
+        pitch: I,
+    ) -> Self {
+        Self {
+            position: position.into(),
+            yaw: yaw.into(),
+            pitch: pitch.into(),
+        }
+    }
+    pub fn calc_matrix(&self) -> cgmath::Matrix4<f32> {
+        let (sin_pitch, cos_pitch) = self.pitch.0.sin_cos();
+        let (sin_yaw, cos_yaw) = self.yaw.0.sin_cos();
+        cgmath::Matrix4::look_to_rh(
+            self.position,
+            Vector3::new(cos_pitch * cos_yaw, sin_pitch, cos_pitch * sin_yaw).normalize(),
+            Vector3::unit_y(),
+        )
+    }
+}
+pub struct Projection {
+    aspect: f32,
+    fovy: Rad<f32>,
+    znear: f32,
+    zfar: f32,
+}
+impl Projection {
+    pub fn new<F: Into<Rad<f32>>>(width: u32, height: u32, fovy: F, znear: f32, zfar: f32) -> Self {
+        Self {
+            aspect: width as f32 / height as f32,
+            fovy: fovy.into(),
+            znear,
+            zfar,
+        }
+    }
+    pub fn resize(&mut self, width: u32, height: u32) {
+        self.aspect = width as f32 / height as f32;
+    }
+    pub fn calc_matrix(&self) -> cgmath::Matrix4<f32> {
+        OPENGL_TO_WGPU_MATRIX * cgmath::perspective(self.fovy, self.aspect, self.znear, self.zfar)
     }
 }
 #[repr(C)]
@@ -39,9 +82,9 @@ impl CameraUniform {
             view_position: [0.0; 4],
         }
     }
-    pub fn update_view_proj(&mut self, camera: &Camera) {
-        self.view_proj = camera.build_view_projection_matrix().into();
-        self.view_position = camera.eye.to_homogeneous().into();
+    pub fn update_view_proj(&mut self, camera: &Camera, projection: &Projection) {
+        self.view_position = camera.position.to_homogeneous().into();
+        self.view_proj = (projection.calc_matrix() * camera.calc_matrix()).into();
     }
 }
 #[derive(PartialEq, Debug, Copy, Clone)]
@@ -50,44 +93,130 @@ pub enum CameraMovement {
     Backward,
     Left,
     Right,
+    Jump,
+    Crouch,
 }
+#[derive(Debug)]
 pub struct CameraController {
+    amount_left: f32,
+    amount_right: f32,
+    amount_forward: f32,
+    amount_backward: f32,
+    amount_up: f32,
+    amount_down: f32,
+    rotate_horizontal: f32,
+    rotate_vertical: f32,
+    scroll: f32,
     speed: f32,
-    position: cgmath::Vector3<f32>,
-    movement: Vec<CameraMovement>,
+    sensitivity: f32,
 }
-impl CameraController {
-    pub fn new(speed: f32) -> Self {
-        Self {
-            speed,
-            position: cgmath::Vector3::new(0.0, 0.0, 0.0),
-            movement: Vec::new(),
-        }
-    }
-    pub fn process_keyboard(&mut self, direction: CameraMovement) {
-        self.movement.push(direction);
-    }
-    pub fn update_camera(&mut self, camera: &mut Camera) {
-        use cgmath::EuclideanSpace;
-        let forward: Vector3<f32> = camera.target.to_vec() - camera.eye.to_vec();
-        let forward_norm = forward.normalize();
-        let forward_mag = forward.magnitude();
-        if self.movement.contains(&CameraMovement::Forward) && forward_mag > self.speed {
-            camera.eye += forward_norm * self.speed;
-        }
-        if self.movement.contains(&CameraMovement::Backward) {
-            camera.eye -= forward_norm * self.speed;
-        }
 
-        let right = forward_norm.cross(camera.up);
-        let forward = camera.target - camera.eye;
-        let forward_mag = forward.magnitude();
-        if self.movement.contains(&CameraMovement::Right) {
-            camera.eye = camera.target - (forward + right * self.speed).normalize() * forward_mag;
+impl CameraController {
+    pub fn new(speed: f32, sensitivity: f32) -> Self {
+        Self {
+            amount_left: 0.0,
+            amount_right: 0.0,
+            amount_forward: 0.0,
+            amount_backward: 0.0,
+            amount_up: 0.0,
+            amount_down: 0.0,
+            rotate_horizontal: 0.0,
+            rotate_vertical: 0.0,
+            scroll: 0.0,
+            speed,
+            sensitivity,
         }
-        if self.movement.contains(&CameraMovement::Left) {
-            camera.eye = camera.target - (forward - right * self.speed).normalize() * forward_mag;
+    }
+    pub fn process_keyboard(&mut self, movement: CameraMovement) -> bool {
+        //reset the movement
+        match movement {
+            CameraMovement::Forward => {
+                self.amount_forward = 1.0;
+                true
+            }
+            CameraMovement::Backward => {
+                self.amount_backward = 1.0;
+                true
+            }
+            CameraMovement::Left => {
+                self.amount_left = 1.0;
+                true
+            }
+            CameraMovement::Right => {
+                self.amount_right = 1.0;
+                true
+            }
+            CameraMovement::Jump => {
+                self.amount_up = 1.0;
+                true
+            }
+            CameraMovement::Crouch => {
+                self.amount_down = 1.0;
+                true
+            }
+            _ => false,
         }
-        self.movement = Vec::new();
+    }
+    pub fn process_mouse(&mut self, mouse_dx: f64, mouse_dy: f64) {
+        self.rotate_horizontal = mouse_dx as f32;
+        self.rotate_vertical = mouse_dy as f32;
+    }
+    pub fn process_scroll(&mut self, delta: &MouseScrollDelta) {
+        self.scroll = -match delta {
+            MouseScrollDelta::LineDelta(_, scroll) => -scroll * 0.5,
+            MouseScrollDelta::PixelDelta(PhysicalPosition { y: scroll, .. }) => -*scroll as f32,
+        };
+    }
+
+    pub fn update_camera(&mut self, camera: &mut Camera, dt: Duration) {
+        let dt = dt.as_secs_f32();
+        // Move forward/backward and left/right
+        let (yaw_sin, yaw_cos) = camera.yaw.0.sin_cos();
+        let forward = Vector3::new(yaw_cos, 0.0, yaw_sin).normalize();
+        let right = Vector3::new(-yaw_sin, 0.0, yaw_cos).normalize();
+        camera.position.z += -(self.amount_forward - self.amount_backward) * self.speed * 10.0 * dt;
+        camera.position.x += (self.amount_right - self.amount_left) * self.speed * 10.0 * dt;
+
+        let (pitch_sin, pitch_cos) = camera.pitch.0.sin_cos();
+        let scrollward =
+            Vector3::new(pitch_cos * yaw_cos, pitch_sin, pitch_cos * yaw_sin).normalize();
+        camera.position += scrollward * self.scroll * self.speed * self.sensitivity * dt;
+        self.scroll = 0.0;
+        camera.position.y += (self.amount_up - self.amount_down) * self.speed * 10.0 * dt;
+        // camera.position.y += (self.amount_up - self.amount_down) * self.speed * 1000.0 * dt;
+        // let yaw = Rad(self.rotate_horizontal * self.sensitivity / 100.0);
+        // let pitch = Rad(-self.rotate_vertical * self.sensitivity / 100.0);
+        // camera.yaw = if yaw == Rad(0.0) { camera.yaw } else { yaw };
+        // camera.pitch = if pitch == Rad(-0.0) {
+        //     camera.pitch
+        // } else {
+        //     pitch
+        // };
+        camera.yaw += Rad(self.rotate_horizontal * self.sensitivity * dt * 10.0);
+        camera.pitch += Rad(-self.rotate_vertical * self.sensitivity * dt * 10.0);
+
+        // if self.rotate_vertical != 0.0 && self.rotate_horizontal != 0.0 {
+        //     println!(
+        //         "yaw and pitch: {:#?} {:#?}",
+        //         Rad(self.rotate_horizontal) * self.sensitivity * dt,
+        //         Rad(-self.rotate_vertical) * self.sensitivity * dt
+        //     );
+        // }
+
+        self.rotate_horizontal = 0.0;
+        self.rotate_vertical = 0.0;
+
+        if camera.pitch < -Rad(SAFE_FRAC_PI_2) {
+            camera.pitch = -Rad(SAFE_FRAC_PI_2);
+        } else if camera.pitch > Rad(SAFE_FRAC_PI_2) {
+            camera.pitch = Rad(SAFE_FRAC_PI_2);
+        }
+        // reset amount
+        self.amount_left = 0.0;
+        self.amount_right = 0.0;
+        self.amount_forward = 0.0;
+        self.amount_backward = 0.0;
+        self.amount_up = 0.0;
+        self.amount_down = 0.0;
     }
 }
--- src/window.rs
+++ src/window.rs
@@ -1,15 +1,18 @@
-use crate::renderer::Renderer;
+use crate::{renderer::Renderer, Plugin};
 use winit::{
-    event::{ElementState, Event, WindowEvent},
+    event::{DeviceEvent, ElementState, Event, WindowEvent},
     event_loop::EventLoop,
     keyboard::Key,
-    platform::modifier_supplement::KeyEventExtModifierSupplement,
-    window::{Fullscreen, WindowBuilder},
+    platform::{
+        modifier_supplement::KeyEventExtModifierSupplement, run_on_demand::EventLoopExtRunOnDemand,
+    },
+    window::{CursorGrabMode, Fullscreen, WindowBuilder},
 };
 
 pub struct Engine {
     renderer: Renderer,
     event_loop: EventLoop<()>,
+    plugins: Vec<Box<dyn Plugin>>,
 }
 impl Engine {
     pub async fn new() -> Engine {
@@ -18,66 +21,124 @@ impl Engine {
             .with_title("Game")
             .build(&event_loop)
             .unwrap();
-        window.set_fullscreen(Some(Fullscreen::Borderless(window.current_monitor())));
+        window.focus_window();
+
+        window.set_cursor_visible(false);
+        #[cfg(target_os = "macos")]
+        window
+            .set_cursor_grab(winit::window::CursorGrabMode::Locked)
+            .unwrap();
+        #[cfg(not(target_os = "macos"))]
+        window.set_cursor_grab(CursorGrabMode::Confined).unwrap();
+
         let renderer = Renderer::new(window).await;
+        let plugins = vec![];
         Engine {
             renderer,
             event_loop,
+            plugins,
         }
         //depending on the platform, use vulkan,opengl or metal
     }
+    pub fn add_plugin(&mut self, plugin: Box<dyn Plugin>) {
+        self.plugins.push(plugin);
+    }
     pub fn run(mut self) {
+        for plugin in self.plugins.iter() {
+            plugin.init();
+        }
         self.event_loop
             .run(move |event, elwt| {
                 let time_now = std::time::Instant::now();
-                if let Event::WindowEvent { event, .. } = event {
-                    match event {
-                        WindowEvent::KeyboardInput { event, .. } => {
-                            if event.state == ElementState::Pressed {
-                                match event.key_without_modifiers().as_ref() {
-                                    Key::Character("w") => {
-                                        self.renderer.camera_controller.process_keyboard(
-                                            crate::camera::CameraMovement::Forward,
-                                        );
+                let mainevent = &event;
+                self.renderer
+                    .window()
+                    .set_fullscreen(Some(Fullscreen::Borderless(
+                        self.renderer.window().current_monitor(),
+                    )));
+                match mainevent {
+                    Event::DeviceEvent { event, .. } => match event {
+                        DeviceEvent::MouseMotion { delta } => {
+                            self.renderer
+                                .camera_controller
+                                .process_mouse(delta.0 as f64, delta.1 as f64);
+                        }
+                        _ => {}
+                    },
+                    Event::WindowEvent { window_id, event } => {
+                        match event {
+                            // WindowEvent::MouseInput { .. } => {
+                            //     if !in_window {
+                            //         self.renderer.cursor_grab();
+                            //         in_window = true;
+                            //     }
+                            // }
+                            WindowEvent::KeyboardInput { event, .. } => {
+                                if event.state == ElementState::Pressed {
+                                    match event.key_without_modifiers().as_ref() {
+                                        Key::Character("w") => {
+                                            self.renderer.camera_controller.process_keyboard(
+                                                crate::camera::CameraMovement::Forward,
+                                            );
+                                        }
+                                        Key::Character("a") => {
+                                            self.renderer.camera_controller.process_keyboard(
+                                                crate::camera::CameraMovement::Left,
+                                            );
+                                        }
+                                        Key::Character("s") => {
+                                            self.renderer.camera_controller.process_keyboard(
+                                                crate::camera::CameraMovement::Backward,
+                                            );
+                                        }
+                                        Key::Character("d") => {
+                                            self.renderer.camera_controller.process_keyboard(
+                                                crate::camera::CameraMovement::Right,
+                                            );
+                                        }
+                                        Key::Named(winit::keyboard::NamedKey::Space) => {
+                                            self.renderer.camera_controller.process_keyboard(
+                                                crate::camera::CameraMovement::Jump,
+                                            );
+                                        }
+                                        Key::Named(winit::keyboard::NamedKey::Shift) => {
+                                            self.renderer.camera_controller.process_keyboard(
+                                                crate::camera::CameraMovement::Crouch,
+                                            );
+                                        }
+                                        _ => {}
                                     }
-                                    Key::Character("a") => {
-                                        self.renderer
-                                            .camera_controller
-                                            .process_keyboard(crate::camera::CameraMovement::Left);
-                                    }
-                                    Key::Character("s") => {
-                                        self.renderer.camera_controller.process_keyboard(
-                                            crate::camera::CameraMovement::Backward,
-                                        );
-                                    }
-                                    Key::Character("d") => {
-                                        self.renderer
-                                            .camera_controller
-                                            .process_keyboard(crate::camera::CameraMovement::Right);
-                                    }
-                                    _ => {}
                                 }
                             }
-                        }
-                        WindowEvent::Resized(physical_size) => {
-                            self.renderer.resize(physical_size);
-                        }
-                        WindowEvent::CloseRequested => {
-                            elwt.exit();
-                        }
+                            WindowEvent::Resized(physical_size) => {
+                                self.renderer.resize(physical_size.clone());
+                            }
+                            WindowEvent::CloseRequested => {
+                                elwt.exit();
+                            }
 
-                        //redraw requested
-                        _ => {}
+                            // WindowEvent::CursorMoved { position, .. } => {
+                            //     self.renderer
+                            //         .camera_controller
+                            //         .process_mouse(position.x as f64, position.y as f64);
+                            // }
+                            WindowEvent::MouseWheel { delta, .. } => {
+                                self.renderer.camera_controller.process_scroll(&delta);
+                            }
+                            //redraw requested
+                            _ => {}
+                        }
                     }
+                    _ => {}
                 }
-                self.renderer.update();
+                self.renderer.update(time_now.elapsed()).unwrap();
                 self.renderer.render();
                 // std::thread::sleep(std::time::Duration::from_millis(16 / 2));
                 let time_elapsed = time_now.elapsed();
                 let avg_fps: f64 = 1.0 / (time_elapsed.as_secs_f64());
                 //only print when build config is debug
-                #[cfg(debug_assertions)]
-                println!("FPS: {}", avg_fps.round());
+                //#[cfg(debug_assertions)]
+                //println!("FPS: {}", avg_fps.round());
             })
             .unwrap();
         //cleanup
